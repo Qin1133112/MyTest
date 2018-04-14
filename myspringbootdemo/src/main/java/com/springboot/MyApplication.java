8d44cf2227c2acd3952cd7699ea62f130a9ff8ca
17503 <17503@promote.cache-dns.local>
2018-04-14 22:41:06 +0800

    spring boot结合ssm、kafka

--- myspringbootdemo/src/main/java/com/springboot/MyApplication.java
+++ myspringbootdemo/src/main/java/com/springboot/MyApplication.java
@@ -1,15 +1,17 @@
-package com.springboot.demo;
+package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class MyspringbootdemoApplication
+@EnableTransactionManagement//开启事务管理
+public class MyApplication
 {
 
 	public static void main(String[] args)
 	{
-		SpringApplication.run(MyspringbootdemoApplication.class, args); 
+		SpringApplication.run(MyApplication.class, args); 
 	}
 }
