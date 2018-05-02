97e62b666b31b0061bf5e918d94514a9d8e60805
17503 <17503@promote.cache-dns.local>
2018-05-03 00:04:18 +0800

    AOP切面

---
+++ myspringbootdemo/src/main/java/com/springboot/aop/LogRecordAspect.java
@@ -0,0 +1,49 @@
+package com.springboot.aop;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.springboot.utils.JackUtils;
+
+//import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;;
+
+
+@Aspect   //定义一个切面
+@Configuration
+public class LogRecordAspect {
+private static final Logger logger = LoggerFactory.getLogger(LogRecordAspect.class);
+
+    // 定义切点Pointcut
+    @Pointcut("execution(* com.springboot..*.controller..*(..))")
+    public void excudeService() {
+    }
+
+    @Around("excudeService()")
+    public Object doAround(ProceedingJoinPoint pjp) throws Throwable {
+    	System.out.println("进来了。。。。");
+        RequestAttributes ra = RequestContextHolder.getRequestAttributes();
+        ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+        HttpServletRequest request = sra.getRequest();
+
+        String url = request.getRequestURL().toString();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+        logger.info("请求开始, 各个参数, url: {}, method: {}, uri: {}, params: {}", url, method, uri, queryString);
+
+        // result的值就是被拦截方法的返回值
+        Object result = pjp.proceed();
+        logger.info("请求结束，controller的返回值是 " + JackUtils.bean2Json(result));
+        return result;
+    }
+}
