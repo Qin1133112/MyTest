c0f192efb3cceb75b65439c891a42a92f8b502e0
17503 <17503@DESKTOP-UU50ILS>
2018-05-10 21:10:36 +0800

    mybatis分页

---
+++ myspringbootdemo/src/main/java/com/springboot/pageUtils/PageBase.java
@@ -0,0 +1,75 @@
+package com.springboot.pageUtils;
+
+public class PageBase
+{
+	private int pageSize = 10;
+	private int pageNo = 0;
+	private long totalNum;
+	private String totalMappedStatementId;
+
+	private long shardValue = 0l;
+
+	public PageBase()
+	{
+		super();
+	}
+
+	public PageBase(int pageSize, int pageNo, int totalNum)
+	{
+		super();
+		this.pageSize = pageSize;
+		this.pageNo = pageNo;
+		this.totalNum = totalNum;
+	}
+
+	public int getPageSize()
+	{
+		return pageSize;
+	}
+
+	public long getShardValue()
+	{
+		return shardValue;
+	}
+
+	public void setShardValue(long shardValue)
+	{
+		this.shardValue = shardValue;
+	}
+
+	public String getTotalMappedStatementId()
+	{
+		return totalMappedStatementId;
+	}
+
+	public void setTotalMappedStatementId(String totalMappedStatementId)
+	{
+		this.totalMappedStatementId = totalMappedStatementId;
+	}
+
+	public void setPageSize(int pageSize)
+	{
+		this.pageSize = pageSize;
+	}
+
+	public int getPageNo()
+	{
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo)
+	{
+		this.pageNo = pageNo;
+	}
+
+	public long getTotalNum()
+	{
+		return totalNum;
+	}
+
+	public void setTotalNum(long totalNum)
+	{
+		this.totalNum = totalNum;
+	}
+
+}
